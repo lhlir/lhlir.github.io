19ccd6f400762c53f6735189fce76020c2c3ccad
Eternal <93047106+lhlir@users.noreply.github.com>
2024-07-17 02:48:06 +0000

    modified:   app.js 	modified:   counterup.js 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -17,9 +17,39 @@ document.addEventListener('DOMContentLoaded', function(){
       var timer = setInterval(function(){olock()}, 1000);
       document.querySelector("audio").setAttribute("src", `music/ido.mp3`);
 
-      document.getElementsByTagName("body")[0].insertAdjacentHTML(
-            "beforeend",
-            "<div id='mask'></div>"
-      );
-
+      // document.getElementsByTagName("body")[0].insertAdjacentHTML(
+      //       "beforeend",
+      //       "<div id='mask'></div>"
+      // );
+      const nicknameInput = document.getElementById('nickname')
+      nicknameInput.addEventListener('input', (e) => {
+            const nickname = e.target.value;
+            // Send the nickname to the GitHub Issues API to create a new issue
+            fetch('https://api.github.com/repos/lhlir/lhlir.github.io/issues', {
+                  method: 'POST',
+                  headers: {
+                        'Authorization': 'Bearer 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',
+                        'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                        title: nickname,
+                        body: ''
+                  })
+            });
+      });
+      function getSavedNicknames() {
+            fetch('https://api.github.com/repos/lhlir/lhlir.github.io/issues').then(response => response.json()).then((issues) => {
+                  const savedNicknameElement = document.getElementById('saved-nickname');
+                  savedNicknameElement.textContent = '';
+                  issues.forEach((issue) => {
+                        const nickname = issue.title;
+                        const nicknameListItem = document.createElement('li');
+                        nicknameListItem.textContent = nickname;
+                        savedNicknameElement.appendChild(nicknameListItem);
+                  });
+            });
+      }       
+      // Call the function to fetch and display saved nicknames
+      getSavedNicknames();
 }, false);
+    
